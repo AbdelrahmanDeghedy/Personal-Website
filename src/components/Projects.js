d8c9531ed280684d9fbea6817f4908daa9c5fe98
Abdelrahman <abdelrahman.deghedy@gmail.com>
2021-05-03 12:48:27 +0200

    Added the project component

---
+++ src/components/Projects.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ProjectCard from './ProjectCard';
+import { projects } from '../utils/_Data';
+
+class Projects extends Component {
+    render () {
+        return (
+            <div className="projects-container">
+                <div className="projects-container-header">
+                    A list of my best projects so far
+                </div>
+                {
+                    Object.keys (projects).map ((pid) => {
+                        return (
+                            <div key={pid} className="projects-item">
+                                <ProjectCard pid={pid}
+                                 name={projects[pid].name}
+                                img={projects[pid].img}
+                                descriptionArray={projects[pid].description}
+                                githubLink={projects[pid].github} 
+                                />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default Projects;
